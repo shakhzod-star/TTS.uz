5579d425e87a9f3e862d6916e17eb7f1dd3d794b
shakhzod-star <77914936+shakhzod-star@users.noreply.github.com>
2023-01-11 20:55:05 +0500

    start

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -10,8 +10,8 @@
                 <p class="font-extralight  text-[18px] leading-[25px] text-center text-darkBlue max-w-[266px]">tts.uz API xizmatidan foydalanish uchun shartnoma qilinadi</p>
             </div>
             <div class="flex flex-col items-center mt-[120px]  relative">
-                <img class="absolute -top-[60px] -left-[230px]" src="/src/assets/icons/Services/left.svg" alt="">
-                <img class="absolute -top-[60px] left-[230px]" src="/src/assets/icons/Services/right.svg" alt="">
+                <img class="absolute -top-[60px] -left-[210px]" src="/src/assets/icons/Services/left.svg" alt="">
+                <img class="absolute -top-[60px] left-[210px]" src="/src/assets/icons/Services/right.svg" alt="">
                 <div class="bg-white rounded-full mb-3"><img class="p-[30px]" src="/src/assets/icons/Services/services2.svg" alt=""></div>
                 <h3 class="font-medium  text-[28px] leading-[59px] text-center mb-2 text-darkBlue">Tariflar & To'lovlar</h3>
                 <p class="font-extralight  text-[18px] leading-[25px] text-center text-darkBlue max-w-[266px]">Shartnoma qilinganidan so'ng ta’rif tanlanadi</p>
