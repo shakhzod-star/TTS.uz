75c4ee2b87e3e8dd01287be82828b013fe8d3fa6
shakhzod-star <77914936+shakhzod-star@users.noreply.github.com>
2023-01-11 21:19:36 +0500

    dd

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import Navbar from '@/views/Navbars.vue'
-import Header from '@/views/Header.vue'
-import Project from '@/views/Project.vue'
-import Telegram from '@/views/Telegram.vue'
-import Services from '@/views/Services.vue'
-import Target from '@/views/Target.vue'
-import Count from '@/views/Count.vue'
-import Footer from '@/views/Footer.vue'
+import Navbar from '../views/Navbars.vue'
+import Header from '../views/Header.vue'
+import Project from '../views/Project.vue'
+import Telegram from '../views/Telegram.vue'
+import Services from '../views/Services.vue'
+import Target from '../views/Target.vue'
+import Count from '../views/Count.vue'
+import Footer from '../views/Footer.vue'
 </script>
 <template>
   <div>
